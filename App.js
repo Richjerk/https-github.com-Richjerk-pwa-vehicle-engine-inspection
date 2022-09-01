04f4b38c913823f2b4e3935adac9ecb4599547eb
Richjerk <49521848+Richjerk@users.noreply.github.com>
2022-09-01 13:29:17 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,11 @@
+var localInspect=require('./NodeInspection.js');
+localInspect.NodeInspection();
+localInspect.NodeInspection.pInspect();
+
+//export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
